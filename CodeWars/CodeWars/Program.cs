36302c16c615a2abc8a37f8c2ca67e2f0875cecb
proninp <proninpaul@yandex.ru>
2018-09-01 18:02:14 +0300

    Adding Big Numbers

--- CodeWars/CodeWars/Program.cs
+++ CodeWars/CodeWars/Program.cs
@@ -11,6 +11,14 @@ namespace CodeWars
 {
     class Program
     {
+        /*
+         * Adding Big Numbers
+         * We need to sum big numbers and we require your help.
+         * Write a function that returns the sum of two numbers. The input numbers are strings and the function must return a string.
+         * Example
+         * add("123", "321"); -> "444"
+         * add("11", "99"); -> "110"
+         */
         public static void Main()
         {
             // Code
