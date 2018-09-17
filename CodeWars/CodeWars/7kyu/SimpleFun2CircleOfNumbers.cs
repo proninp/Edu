f4070612ad493f8fc75e560eba928586a6a29bc4
proninp <proninpaul@yandex.ru>
2018-09-17 23:12:20 +0300

    Simple Fun #2: Circle of Numbers

---
+++ CodeWars/CodeWars/7kyu/SimpleFun2CircleOfNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeWars._7kyu
+{
+    /*
+     * Simple Fun #2: Circle of Numbers
+     * 
+     * Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two
+     * neighbouring numbers is equal (note that 0 and n - 1 are neighbouring, too).
+     * Given n and firstNumber/first_number, find the number which is written in the radially opposite position to firstNumber.
+     * Example
+     * For n = 10 and firstNumber = 2, the output should be
+     * circleOfNumbers(n, firstNumber) === 7
+     * circleOfNumbers(n, firstNumber); // --> 7
+     * circle_of_numbers($n, $first_number); // -> 7
+     */
+    public class SimpleFun2CircleOfNumbers
+    {
+        public static int CircleOfNumbers(int n, int FirstNumber)
+        {
+            return (n / 2 + FirstNumber) % n;
+        }
+    }
+}
