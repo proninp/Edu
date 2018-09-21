962b1c1848c8e1527bed080ec54f78057d757099
proninp <proninpaul@yandex.ru>
2018-09-21 23:59:19 +0300

    Isogram

---
+++ CodeWars/CodeWars/7kyu/Isograms.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace CodeWars._7kyu
+{
+    /*
+     * Isograms
+     * An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+     * Implement a function that determines whether a string that contains only letters is an isogram.
+     * Assume the empty string is an isogram. Ignore letter case.
+     * isIsogram "Dermatoglyphics" == true
+     * isIsogram "moose" == false
+     * isIsogram "aba" == false
+     */
+    public class Isograms
+    {
+        public static bool IsIsogram(string str) => str.Length > 0 ? str.ToLower().GroupBy(c => c).Select(c => c.Count()).Max() == 1 : true;
+    }
+}
