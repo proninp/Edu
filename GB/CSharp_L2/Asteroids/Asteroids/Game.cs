6e1416b59db683c6bdbb7f0c3fa88d8121e29f46
proninp <proninpaul@yandex.ru>
2018-10-02 23:13:29 +0300

    Asteroids Game

---
+++ GB/CSharp_L2/Asteroids/Asteroids/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Asteroids
+{
+    static class Game
+    {
+        static BufferedGraphicsContext context;
+        static BufferedGraphics Buffer;
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        static Game()
+        {
+
+        }
+        public static void Init(Form form)
+        {
+            Graphics g = form.CreateGraphics();
+            context = BufferedGraphicsManager.Current;
+            Width = form.Width;
+            Height = form.Height;
+            Buffer = context.Allocate(g, new Rectangle(0, 0, Width, Height));
+        }
+        public static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+            Buffer.Graphics.DrawRectangle(Pens.Wheat, new Rectangle(100, 100, 200, 200));
+            Buffer.Render();
+        }
+
+    }
+}
---
+++ GB/CSharp_L2/Asteroids/Asteroids/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+
+        }
+    }
+}
