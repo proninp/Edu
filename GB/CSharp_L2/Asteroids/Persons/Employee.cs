94b7e6c4e52840400ca52d791bfbf9c34fac8070
proninp <proninpaul@yandex.ru>
2018-11-11 17:40:24 +0300

    Perosns

---
+++ GB/CSharp_L2/Asteroids/Persons/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons
+{
+    class Employee : Person
+    {
+        public double Salary { get; set; }
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="surname">Фамилия</param>
+        /// <param name="age">Возраст</param>
+        public Employee(string name, string surname, int age) : base(name, surname, age){ }
+        /// <summary>
+        /// Конструктор с ЗП
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="surname">Фаимилия</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="salary">Зарплата</param>
+        public Employee(string name, string surname, int age, double salary) : this(name, surname, age)
+        {
+            Salary = salary;
+        }
+        /// <summary>
+        /// переопределенный метод получения средней зарплаты
+        /// </summary>
+        /// <returns>Средняя ЗП</returns>
+        public override double AvgSalary() => Salary;
+    }
+}
---
+++ GB/CSharp_L2/Asteroids/Persons/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="surname">Фамилия</param>
+        /// <param name="age">Возраст</param>
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+        /// <summary>
+        /// Метод для расчета среднемесячной заработной платы
+        /// </summary>
+        /// <returns></returns>
+        public abstract double AvgSalary();
+        /// <summary>
+        /// Инфорация
+        /// </summary>
+        public abstract void GetInfo();
+    }
+}
