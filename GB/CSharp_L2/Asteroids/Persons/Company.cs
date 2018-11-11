b9074f54712f0c96c27b2755cdf9532fdcace667
proninp <proninpaul@yandex.ru>
2018-11-11 18:26:10 +0300

    Company

---
+++ GB/CSharp_L2/Asteroids/Persons/Company.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Persons
+{
+    public class Company : IEnumerable
+    {
+        public List<Person> Department { get; set; }
+        public Company(List<Person> list)
+        {
+            Department = list;
+        }
+        public Company() : this(new List<Person>()){ }
+        /// <summary>
+        /// Реализация метода GetEnumerator() интерфейса IEnumerator
+        /// </summary>
+        /// <returns>Экземпляр Person</returns>
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var d in Department)
+                yield return d.ToString();
+        }
+    }
+}
---
+++ GB/CSharp_L2/Asteroids/Persons/Outsourcer.cs
@@ -0,0 +1,30 @@
+﻿namespace Persons
+{
+    class Outsourcer : Person
+    {
+        public double SalaryPH { get; set; }
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="surname">Фамилия</param>
+        /// <param name="age">Возраст</param>
+        public Outsourcer(string name, string surname, int age) : base(name, surname, age){ }
+        /// <summary>
+        /// Конструктор с ЗП
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="surname">Фамилия</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="salary">Зарплата</param>
+        public Outsourcer(string name, string surname, int age, double salaryPH) : this(name, surname, age)
+        {
+            SalaryPH = salaryPH;
+        }
+        /// <summary>
+        /// Переопределенный метод получения средней зарплаты
+        /// </summary>
+        /// <returns>Средняя ЗП</returns>
+        public override double AvgSalary() => SalaryPH * 20.8 * 8;
+    }
+}
