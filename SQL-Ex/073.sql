f54f4fc4981cfd4e54fe82a45481772957c4d489
Pavel <proninpaul@yandex.ru>
2020-03-25 08:42:05 +0300

    Add second option

--- SQL-Ex/073.sql
+++ SQL-Ex/073.sql
@@ -15,3 +15,23 @@ EXCEPT
     FROM Outcomes o
          JOIN Classes c ON c.class = o.ship
 );
+
+/* Option 2 */
+
+SELECT country,
+       b.name
+FROM Classes c
+CROSS JOIN Battles b
+EXCEPT (
+    SELECT country,
+           battle
+    FROM (
+	   SELECT ship class,
+			battle
+	   FROM Outcomes
+	   UNION
+	   SELECT s.class,
+			battle
+	   FROM Outcomes o
+	   JOIN Ships s ON s.name = o.ship) q
+    JOIN Classes c ON c.class = q.class)
