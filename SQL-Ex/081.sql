aa0209a20f914a000156d08f02cf43a143aee505
Pavel <proninpaul@yandex.ru>
2020-04-05 10:36:13 +0300

    Add second option

--- SQL-Ex/081.sql
+++ SQL-Ex/081.sql
@@ -1,14 +1,20 @@
-WITH summary
-     AS (SELECT TOP 1 WITH TIES DATEPART(month, date) month_Val,
-                                DATEPART(year, date) year_Val,
-                                SUM(out) max_Sum
-         FROM Outcome
-         GROUP BY DATEPART(month, date),
-                  DATEPART(year, date) ORDER BY max_Sum DESC)
-     SELECT o.code,
-            o.point,
-            o.date,
-            o.out
-     FROM Outcome o
-          JOIN summary s ON s.month_Val = DATEPART(month, o.date)
-                            AND s.year_Val = DATEPART(year, date);
+SELECT o.code
+       ,o.point
+       ,o.date
+       ,o.out
+FROM Outcome o
+JOIN (
+    SELECT TOP 1 WITH TIES DATEPART(m,[date]) m
+		 ,DATEPART(yyyy,[date]) y
+		 ,SUM(out) am
+    FROM Outcome
+    GROUP BY DATEPART(m,[date]), DATEPART(yyyy,[date])
+    ORDER BY am DESC) q
+ON q.m = DATEPART(m,o.[date]) AND q.y = DATEPART(yyyy,o.[date])
+
+
+/* Option 2 */
+
+SELECT TOP 1 WITH TIES code, point, date, out
+FROM Outcome
+ORDER BY SUM(out) OVER(PARTITION BY YEAR(date), MONTH(date)) DESC
