3f5df1e32c5dcf48247d0a4881a0627f7c02f1e2
Pavel <proninpaul@yandex.ru>
2020-04-05 10:19:00 +0300

    Add second option

--- SQL-Ex/080.sql
+++ SQL-Ex/080.sql
@@ -12,3 +12,13 @@ FROM Product p
     SELECT model
     FROM PC
 ) m ON m.Model = p.Model;
+
+/* Option 2 */
+
+SELECT maker
+FROM Product
+EXCEPT
+SELECT maker
+FROM product p
+LEFT JOIN pc ON pc.model = p.model
+WHERE type = 'PC' AND pc.model IS NULL
