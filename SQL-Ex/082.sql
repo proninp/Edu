dfcbf737ab62fc44350fe7c450f14be6b9375e73
Pavel <proninpaul@yandex.ru>
2020-04-12 10:57:23 +0300

    SQL-ex Tasks (T-SQL) 082

--- SQL-Ex/082.sql
+++ SQL-Ex/082.sql
@@ -9,3 +9,9 @@ FROM sub a
 JOIN sub b ON a.num <= b.num AND b.num <= a.num + 5
 WHERE a.num <= ((SELECT MAX(num) FROM sub) - 5)
 GROUP BY a.code
+
+-- second option
+SELECT TOP(SELECT COUNT(*)-5 from PC)
+MIN(code) OVER (ORDER BY code ROWS BETWEEN CURRENT ROW AND 5 FOLLOWING)  gr_code,
+AVG(price) OVER (ORDER BY code ROWS BETWEEN CURRENT ROW AND 5 FOLLOWING) avg_price
+FROM PC
