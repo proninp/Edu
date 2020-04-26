520a4c5b60aaf4d74276403b48ab1891cc4d6572
Pavel <proninpaul@yandex.ru>
2020-04-26 16:03:00 +0300

    Module 8: Grouping Sets and Pivoting Data

--- edX/Microsoft DAT201x - Querying with Transact-SQL/Module 08 - Grouping Sets and Pivoting Data.sql
+++ edX/Microsoft DAT201x - Querying with Transact-SQL/Module 08 - Grouping Sets and Pivoting Data.sql
@@ -1,6 +1,6 @@
---Modify the query so that the results include a grand total for all sales revenue and a subtotal for each
---country/region in addition to the state/province subtotals that are already returned.
---Make sure to use the aliases provided, and default column names elsewhere.
+/*Modify the query so that the results include a grand total for all sales revenue and a subtotal for each
+country/region in addition to the state/province subtotals that are already returned.
+Make sure to use the aliases provided, and default column names elsewhere.*/
 
 SELECT a.CountryRegion, a.StateProvince, SUM(soh.TotalDue) AS Revenue
 FROM SalesLT.Address AS a
@@ -15,11 +15,11 @@ GROUP BY a.CountryRegion, a.StateProvince WITH ROLLUP
 ORDER BY a.CountryRegion, a.StateProvince;
 
 
---Modify your query to include a column named Level that indicates at which level in the total, country/region,
---and state/province hierarchy the revenue figure in the row is aggregated.
---For example, the grand total row should contain the value 'Total', the row showing the subtotal for United States should contain
---the value 'United States Subtotal', and the row showing the subtotal for California should contain the value 'California Subtotal'.
---Make sure to use the aliases provided, and default column names elsewhere.
+/*Modify your query to include a column named Level that indicates at which level in the total, country/region,
+and state/province hierarchy the revenue figure in the row is aggregated.
+For example, the grand total row should contain the value 'Total', the row showing the subtotal for United States should contain
+the value 'United States Subtotal', and the row showing the subtotal for California should contain the value 'California Subtotal'.
+Make sure to use the aliases provided, and default column names elsewhere.*/
 
 SELECT a.CountryRegion, a.StateProvince,
 IIF(GROUPING_ID(a.CountryRegion) = 1 AND GROUPING_ID(a.StateProvince) = 1, 'Total', IIF(GROUPING_ID(a.StateProvince) = 1, a.CountryRegion + ' Subtotal', a.StateProvince + ' Subtotal')) AS Level,
@@ -35,7 +35,8 @@ GROUP BY ROLLUP(a.CountryRegion, a.StateProvince)
 ORDER BY a.CountryRegion, a.StateProvince;
 
 
---Extend your query to include a grouping for individual cities. Make sure to use the aliases provided, and default column names elsewhere.
+/*Extend your query to include a grouping for individual cities.
+Make sure to use the aliases provided, and default column names elsewhere.*/
 
 SELECT a.CountryRegion, a.StateProvince, a.City,
 CHOOSE (1 + GROUPING_ID(a.CountryRegion) + GROUPING_ID(a.StateProvince) + GROUPING_ID(a.City),
@@ -53,8 +54,8 @@ GROUP BY ROLLUP(a.CountryRegion, a.StateProvince, a.City)
 ORDER BY a.CountryRegion, a.StateProvince, a.City;
 
 
---Retrieve a list of customer company names together with their total revenue for each parent category in Accessories, Bikes, Clothing, and Components.
---Make sure to use the aliases provided, and default column names elsewhere.
+/*Retrieve a list of customer company names together with their total revenue for each parent category in Accessories, Bikes, Clothing, and Components.
+Make sure to use the aliases provided, and default column names elsewhere.*/
 SELECT * FROM
 (SELECT cat.ParentProductCategoryName, cust.CompanyName, sod.LineTotal
  FROM SalesLT.SalesOrderDetail AS sod
