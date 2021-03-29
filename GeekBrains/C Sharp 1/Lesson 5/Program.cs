aba7e62e74b6d7f06986a6a7a06ec0374bb9cba0
Pavel <proninpaul@yandex.ru>
2021-03-29 19:46:33 +0300

    Lesson 05

---
+++ GeekBrains/C Sharp 1/Lesson 5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1.Go();
+            //Task2.Go();
+            //Task3.Go();
+            Task4.Go();
+            Console.ReadLine();
+        }
+    }
+}
