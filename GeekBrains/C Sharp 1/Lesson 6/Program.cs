cd6b2aeca725ac63fdffd6cd1a1e3bacd469f971
Pavel <proninpaul@yandex.ru>
2021-04-03 16:53:40 +0300

    Lesson 6

---
+++ GeekBrains/C Sharp 1/Lesson 6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1.Go();
+            Task2.Go();
+            Console.ReadLine();
+        }
+    }
+}
