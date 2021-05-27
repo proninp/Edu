3cbe2a724b1b62c3e5b41affa6cda6b76024428f
Pavel <proninpaul@yandex.ru>
2021-05-27 08:21:11 +0300

    C Sharp Kata 7 - String ends with

---
+++ CodeWars/C Sharp/CodeWars/7kyu/StringEndsWith.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars._7kyu
+{
+    /*
+     * Complete the solution so that it returns true if the first argument(string)
+     * passed in ends with the 2nd argument (also a string).
+     * strEndsWith('abc', 'bc') -- returns true
+     * strEndsWith('abc', 'd') -- returns false
+     */
+    public class StringEndsWith
+    {
+        public static bool Solution(string str, string ending) => str.EndsWith(ending);
+    }
+}
