40fc1f17d6633b3c8823e9fa2fca6ef7ffb4bc99
Pavel <proninpaul@yandex.ru>
2022-04-23 18:10:18 +0300

    LeetCode 13

---
+++ LeetCode/LeetCode/Easy/Roman_to_integer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class Roman_to_integer
+    {
+        public static int RomanToInt(string s)
+        {
+            StringBuilder builder = new StringBuilder(s.Length);
+            builder.Append(s);
+            StringBuilder stringBuilder = new StringBuilder(2);
+            int result = 0;
+            string[] romanArray = new string[] { "I", "V", "X", "L", "C", "D", "M"};
+            int[] numericArray = new int[] { 1, 5, 10, 50, 100, 500, 1000 };
+            string[] romanSecArray = new string[] { "IV", "IX", "XL", "XC", "CD", "CM" };
+            int[] numericSecArray = new int[] { 4, 9, 40, 90, 400, 900 };
+            
+            while (builder.Length > 0)
+            {
+                int numeric = 0;
+                if (builder.Length > 1)
+                {
+                    stringBuilder.Clear();
+                    stringBuilder.Append(builder.ToString(0, 2));
+                    for (int i = 0; i < romanSecArray.Length; i++)
+                        if (stringBuilder.ToString().Equals(romanSecArray[i]))
+                            numeric = numericSecArray[i];
+                }
+                if (numeric == 0)
+                {
+                    stringBuilder.Clear();
+                    stringBuilder.Append(builder.ToString(0, 1));
+                    for (int i = 0; i < romanArray.Length; i++)
+                        if (stringBuilder.ToString().Equals(romanArray[i]))
+                                numeric = numericArray[i];
+                }
+                builder.Remove(0, stringBuilder.Length);
+                result += numeric;
+            }
+            return result;
+        }
+   }
+}
---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Easy.Roman_to_integer.RomanToInt("III"));
+            Console.WriteLine(Easy.Roman_to_integer.RomanToInt("LVIII"));
+            Console.WriteLine(Easy.Roman_to_integer.RomanToInt("MCMXCIV"));
+            Console.ReadLine();
+        }
+    }
+}
