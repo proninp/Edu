66dd597a42f872cf8b23faef0d0c5e166a459fe1
Pavel <proninpaul@yandex.ru>
2022-09-14 08:58:45 +0300

    LeetCode Java Palindrome_Number

--- LeetCode/Java LeetCode/src/com/leetcode/problems/Main.java
+++ LeetCode/Java LeetCode/src/com/leetcode/problems/Main.java
@@ -1,7 +1,9 @@
 package com.leetcode.problems;
+import com.leetcode.problems.*;
+import com.leetcode.problems.Palindrome_Number.Palindrome_Number;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Palindrome_Number.isPalindrome(654456));
     }
 }
