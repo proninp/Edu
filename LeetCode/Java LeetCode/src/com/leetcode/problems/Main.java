601cfb17018c4b7c13dcbe030fa7eba00850f211
Pavel <proninpaul@yandex.ru>
2022-09-16 09:12:12 +0300

    LeetCode Java Reverse_Integer

--- LeetCode/Java LeetCode/src/com/leetcode/problems/Main.java
+++ LeetCode/Java LeetCode/src/com/leetcode/problems/Main.java
@@ -1,9 +1,11 @@
 package com.leetcode.problems;
 import com.leetcode.problems.*;
 import com.leetcode.problems.Palindrome_Number.Palindrome_Number;
+import com.leetcode.problems.Reverse_Integer.Reverse_Integer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Palindrome_Number.isPalindrome(654456));
+        //System.out.println(Reverse_Integer.reverse(Integer.MAX_VALUE));
+        System.out.println(Reverse_Integer.reverse(1463847412));
     }
 }
