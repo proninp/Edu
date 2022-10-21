64046df5d970312b209edfbe829011c3711b547b
proninp <proninpaul@yandex.ru>
2022-10-21 08:31:33 +0300

    Java 7kyu.Fibonacci

---
+++ CodeWars/Java/CodeWars/src/Main.java
@@ -0,0 +1,8 @@
+import com.codewars.kyu7.*;
+public class Main {
+    public static void main(String[] args) {
+        int a = 17;
+        System.out.println(Fibonacci.Fib(a));
+        System.out.println(Fibonacci.FibRecursion(a));
+    }
+}
