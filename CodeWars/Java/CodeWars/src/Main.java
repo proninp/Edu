d3771fb6551c1e5a698dde277e874cceffd43e66
proninp <proninpaul@yandex.ru>
2022-10-22 08:42:42 +0300

    Java 7kyu.Vowel Count

--- CodeWars/Java/CodeWars/src/Main.java
+++ CodeWars/Java/CodeWars/src/Main.java
@@ -1,8 +1,7 @@
 import com.codewars.kyu7.*;
 public class Main {
     public static void main(String[] args) {
-        int a = 1243;
-        System.out.println(DescendingOrder.sortDesc(a));
-        System.out.println(DescendingOrder.sortDescStr(a));
+        String test = "pear tree";
+        System.out.println(VowelCount.getCount(test));
     }
 }
