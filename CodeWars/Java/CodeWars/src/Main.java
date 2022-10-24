1723cbe9db23f19bee4e05b94c3649b16434981a
proninp <proninpaul@yandex.ru>
2022-10-24 09:20:00 +0300

    Java 7kyu.Binaty Addition

--- CodeWars/Java/CodeWars/src/Main.java
+++ CodeWars/Java/CodeWars/src/Main.java
@@ -1,9 +1,6 @@
 import com.codewars.kyu7.*;
 public class Main {
     public static void main(String[] args) {
-        String test = "Hello world!";
-        String test2 = CircleCipher.encode(test);
-        System.out.println(test2);
-        System.out.println(CircleCipher.decode(test2));
+        System.out.println(BinaryAddition.binaryAddition(5, 2));
     }
 }
