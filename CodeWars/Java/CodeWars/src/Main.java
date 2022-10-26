2ccd041c3a288b2efa1477b87a9f27b11e710e87
proninp <proninpaul@yandex.ru>
2022-10-26 08:54:52 +0300

    Java 7kyu.Two to One

--- CodeWars/Java/CodeWars/src/Main.java
+++ CodeWars/Java/CodeWars/src/Main.java
@@ -2,6 +2,6 @@ import com.codewars.kyu7.*;
 import com.codewars.kyu5.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(MaximumSubarraySum.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(TwoToOne.longest("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz"));
     }
 }
