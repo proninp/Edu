61246d493947759f7b1f24c22eb6bbe2b9fdc746
proninp <proninpaul@yandex.ru>
2022-10-27 09:10:01 +0300

    Java 5kyu.CamelCase Method

--- CodeWars/Java/CodeWars/src/Main.java
+++ CodeWars/Java/CodeWars/src/Main.java
@@ -1,7 +1,8 @@
+import com.codewars.kyu6.CamelCaseMethod;
 import com.codewars.kyu7.*;
 import com.codewars.kyu5.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(TwoToOne.longest("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz"));
+        System.out.println(CamelCaseMethod.camelCase(""));
     }
 }
---
+++ CodeWars/Java/CodeWars/src/com/codewars/kyu6/CamelCaseMethod.java
@@ -0,0 +1,30 @@
+/*
+ * Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java)
+ * for strings. All words must have their first letter capitalized without spaces.
+ */
+
+package com.codewars.kyu6;
+
+import java.util.Arrays;
+
+import static java.util.stream.Collectors.joining;
+
+public class CamelCaseMethod {
+    public static String camelCase(String str) {
+        if (str.isEmpty())
+            return str;
+        String[] strArr = str.trim().replaceAll("\\s+", " ").split(" ");
+        for (int i = 0; i < strArr.length; i++) {
+            if (!strArr[i].isEmpty())
+                strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+        }
+        return String.join("", strArr);
+    }
+    public static String camelCase2(final String string) {
+
+        return Arrays.stream(string.split("\\s+"))
+                .filter(s -> !s.isEmpty())
+                .map(s -> Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase())
+                .collect(joining());
+    }
+}
