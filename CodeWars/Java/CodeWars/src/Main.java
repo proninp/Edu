3b5c2adf69b4910aa14d91011660beaf8eb06974
proninp <proninpaul@yandex.ru>
2022-10-28 08:19:39 +0300

    Java 6kyu.Sum of Parts

--- CodeWars/Java/CodeWars/src/Main.java
+++ CodeWars/Java/CodeWars/src/Main.java
@@ -1,8 +1,12 @@
 import com.codewars.kyu6.CamelCaseMethod;
+import com.codewars.kyu6.SumOfParts;
 import com.codewars.kyu7.*;
 import com.codewars.kyu5.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(CamelCaseMethod.camelCase(""));
+        int[] a = SumOfParts.sumParts(new int[] {1, 2, 3, 4, 5, 6});
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 }
