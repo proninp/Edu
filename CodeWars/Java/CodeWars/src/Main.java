e4b90869039dd6aa9e3fdfcabb9297dd8e1b2f0b
proninp <proninpaul@yandex.ru>
2022-10-29 15:59:42 +0300

    Java 6kyu.Write Number in Expanded Form

--- CodeWars/Java/CodeWars/src/Main.java
+++ CodeWars/Java/CodeWars/src/Main.java
@@ -1,12 +1,10 @@
 import com.codewars.kyu6.CamelCaseMethod;
 import com.codewars.kyu6.SumOfParts;
+import com.codewars.kyu6.WriteNumberInExpandedForm;
 import com.codewars.kyu7.*;
 import com.codewars.kyu5.*;
 public class Main {
     public static void main(String[] args) {
-        int[] a = SumOfParts.sumParts(new int[] {1, 2, 3, 4, 5, 6});
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
+        System.out.println(WriteNumberInExpandedForm.expandedForm(4620450));
     }
 }
