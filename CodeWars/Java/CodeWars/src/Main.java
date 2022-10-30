84471d570eb4cad6995eaaa00fd284d4b03792b1
proninp <proninpaul@yandex.ru>
2022-10-30 11:13:38 +0300

    Java 6kyu.Take a Number And Sum Its Digits Raised To The Consecutive

--- CodeWars/Java/CodeWars/src/Main.java
+++ CodeWars/Java/CodeWars/src/Main.java
@@ -1,10 +1,16 @@
 import com.codewars.kyu6.CamelCaseMethod;
+import com.codewars.kyu6.Kata5626b561280a42ecc50000d1;
 import com.codewars.kyu6.SumOfParts;
 import com.codewars.kyu6.WriteNumberInExpandedForm;
 import com.codewars.kyu7.*;
 import com.codewars.kyu5.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(WriteNumberInExpandedForm.expandedForm(4620450));
+        Kata5626b561280a42ecc50000d1.sumDigPow(1L, 100L);
+
     }
 }
