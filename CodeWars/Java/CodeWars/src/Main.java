36a39015e4c3059bac19fe9cf5a00b6d1e416c70
proninp <proninpaul@yandex.ru>
2022-11-04 11:04:22 +0300

    Java 5kyu.Secure Tester

--- CodeWars/Java/CodeWars/src/Main.java
+++ CodeWars/Java/CodeWars/src/Main.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(BackspacesInString.cleanString("abc#d##c"));
+        System.out.println(SecureTester.alphanumeric("nave"));
     }
 }
