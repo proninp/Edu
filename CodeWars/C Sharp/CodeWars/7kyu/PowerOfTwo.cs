d1218a1f3d13823e0d91a70ec3ea13734531a531
proninp <proninpaul@yandex.ru>
2023-04-10 08:42:09 +0300

    Codewars - 7 kyu - Power of two

---
+++ CodeWars/C Sharp/CodeWars/7kyu/PowerOfTwo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars._7kyu
+{
+    /*
+     * Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language)
+     * that determines if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
+     * 
+     * a power of two is a number of the form 2n where n is an integer,
+     * i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+     */
+    internal class PowerOfTwo
+    {
+        public static bool PowerOfTwoMethod(int n)
+        {
+            if (n == 1)
+                return true;
+            else if (n % 2 != 0 || n == 0)
+                return false;
+            return PowerOfTwoMethod(n / 2);
+        }
+    }
+}
