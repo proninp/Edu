bb052028edfe1e24944af471dc2b54d2320128a0
proninp <proninpaul@yandex.ru>
2023-04-13 23:17:04 +0300

    GB Algorithms Lesson 02 - 01

---
+++ GeekBrains/Algorithms/2023/Lesson02/Lesson02/ILinkedList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    internal interface ILinkedList
+    {
+        int GetCount(); // возвращает количество элементовв списке
+        void AddNode(int value); // добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); // добавляетновый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанныйэлемент
+        Node FindNode(int searchValue); // ищет элемент по его значению
+    }
+}
