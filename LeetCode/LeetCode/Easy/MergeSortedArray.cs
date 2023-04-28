46d0b5259e773887f42775cea5eee40a5a42db6d
proninp <proninpaul@yandex.ru>
2023-04-28 08:51:57 +0300

    LeetCode C# 88. Merge Sorted Array vol.2

--- LeetCode/LeetCode/Easy/MergeSortedArray.cs
+++ LeetCode/LeetCode/Easy/MergeSortedArray.cs
@@ -34,5 +34,11 @@ namespace LeetCode.Easy
                     nums1[i] = (n1[p1] <= nums2[p2]) ? n1[p1++] : nums2[p2++];
             }
         }
+        public void Merge2(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = m; i < (m + n); i++)
+                nums1[i] = nums2[i - m];
+            Array.Sort(nums1);
+        }
     }
 }
