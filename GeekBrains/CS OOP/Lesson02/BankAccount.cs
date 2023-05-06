ff589d11a5f482a8d9f89d0ba177a108f4f26f23
proninp <proninpaul@yandex.ru>
2023-05-06 18:12:38 +0300

    Revert "Revert "C# OOP Lesson 02 p.1""
    This reverts commit 2d9e5e31da484bcfea11c51d6652c031106e03a5.

---
+++ GeekBrains/CS OOP/Lesson02/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    enum BankAccountType: ushort
+    {
+        None = 0,
+        Debit = 1,
+        Credit = 2
+    }
+    internal class BankAccount
+    {
+        private static int bankAccountEntryNo = 0;
+        private int accountNumber;
+        private decimal balance;
+        private BankAccountType bankAccountType;
+        public int AccountNumber { get; }
+        public BankAccountType BankAccountType { get; }
+        public Decimal Balance { get; set; }
+        
+
+    }
+}
--- LeetCode/LeetCode/Easy/MaximumDepthOfBinaryTree.cs
+++ LeetCode/LeetCode/Easy/MaximumDepthOfBinaryTree.cs
@@ -16,19 +16,19 @@ namespace LeetCode.Easy
      */
     internal class MaximumDepthOfBinaryTree
     {
-        public int MaxDepth(TreeNode root)
+        public int MaxDepth(TreeNode104 root)
         {
             if (root == null)
                 return 0;
             return (1 + (Math.Max(MaxDepth(root.left), MaxDepth(root.right))));
         }
     }
-    public class TreeNode
+    public class TreeNode104
     {
         public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        public TreeNode104 left;
+        public TreeNode104 right;
+        public TreeNode104(int val = 0, TreeNode104 left = null, TreeNode104 right = null)
         {
             this.val = val;
             this.left = left;
