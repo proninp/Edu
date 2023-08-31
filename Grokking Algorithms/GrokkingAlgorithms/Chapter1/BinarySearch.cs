237a91d31bc4780dbaacb74f9a7f0699820409a3
proninp <proninpaul@yandex.ru>
2023-08-31 09:32:40 +0300

    Grokking Algorithms C# Binary search algorithm

---
+++ Grokking Algorithms/GrokkingAlgorithms/Chapter1/BinarySearch.cs
@@ -0,0 +1,24 @@
+﻿namespace Chapter1
+{
+    public class BinarySearch
+    {
+        public static int BinSearch(int[] array, int item)
+        {
+            var low = 0;
+            var high = array.Length - 1;
+            int mid, guess;
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                guess = array[mid];
+                if (guess == item)
+                    return mid;
+                if (guess > item)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            return -1;
+        }
+    }
+}
