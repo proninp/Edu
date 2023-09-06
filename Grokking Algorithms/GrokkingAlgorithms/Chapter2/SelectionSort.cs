8ebcaa8d2cae17c7a43751e60a54507fdcb96b02
proninp <proninpaul@yandex.ru>
2023-09-06 18:45:56 +0300

    Grokking Algorithms C# Selection Sort algorithm

---
+++ Grokking Algorithms/GrokkingAlgorithms/Chapter2/SelectionSort.cs
@@ -0,0 +1,17 @@
+﻿namespace Chapter2
+{
+    public class SelectionSort
+    {
+        public static void SelectSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[i])
+                        (arr[i], arr[j]) = (arr[j], arr[i]);
+                }
+            }
+        }
+    }
+}
