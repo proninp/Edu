33c7002a779eaf7adf4fc6ed2ba2635bbdd73390
proninp <proninpaul@yandex.ru>
2023-12-06 16:42:13 +0300

    C# - 7 kyu - Small enough? - Beginner

---
+++ CodeWars/C Sharp/CodeWars/7kyu/SmallEnoughBeginner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars._7kyu
+{
+    /*
+     * Small enough? - Beginner
+     * 
+     * You will be given an array and a limit value.
+     * You must check that all values in the array are below or equal to the limit value.
+     * If they are, return true. Else, return false.
+     * You can assume all values in the array are numbers.
+     */
+    public class SmallEnoughBeginner
+    {
+        public static bool SmallEnough(int[] a, int limit)
+        {
+            return a.All(x => x <= limit);
+        }
+    }
+}
