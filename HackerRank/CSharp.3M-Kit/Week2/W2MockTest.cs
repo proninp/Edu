5f2e45fc9cc3d6c9f58d0c8d19f2152898751602
proninp <proninpaul@yandex.ru>
2024-01-21 12:46:35 +0300

    HackerRank - CSharp - 3M.Week2 - Mock Test

---
+++ HackerRank/CSharp.3M-Kit/Week2/W2MockTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Week2;
+
+public class W2MockTest
+{
+    public static int FlippingMatrix(List<List<int>> matrix)
+    {
+        int len = matrix.Count / 2;
+        int max, maxSum = 0;
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                max = int.MinValue;
+                max = Math.Max(matrix[i][j], max);
+                max = Math.Max(matrix[len * 2 - 1 - i][j], max);
+                max = Math.Max(matrix[i][len * 2 - 1 - j], max);
+                max = Math.Max(matrix[len * 2 - 1 - i][len * 2 - 1 - j], max);
+                maxSum += max;
+            }
+        }
+        return maxSum;
+    }
+}
