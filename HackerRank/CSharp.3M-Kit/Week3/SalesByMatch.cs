662e1ac1fe31fb0ff746c334a92bab3631b67794
proninp <proninpaul@yandex.ru>
2024-01-24 09:09:41 +0300

    HackerRank - CSharp - 3M.Week3 - Sales by Match

---
+++ HackerRank/CSharp.3M-Kit/Week3/SalesByMatch.cs
@@ -0,0 +1,34 @@
+﻿namespace Week3;
+
+/*
+ * Sales by Match
+ * 
+ * There is a large pile of socks that must be paired by color. Given an array of integers representing the color of
+ * each sock, determine how many pairs of socks with matching colors there are.
+ * 
+ * Function Description
+ * Complete the SockMerchant function in the editor below.
+ * SockMerchant has the following parameter(s):
+ * int n: the number of socks in the pile
+ * int ar[n]: the colors of each sock
+ * Returns
+ * int: the number of pairs
+ */
+
+public class SalesByMatch
+{
+    public static int SockMerchant(int n, List<int> ar)
+    {
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < ar.Count; i++)
+        {
+            if (!map.ContainsKey(ar[i]))
+                map[ar[i]] = 0;
+            map[ar[i]]++;
+        }
+        var pairsCount = 0;
+        foreach (var e in map.Values)
+            pairsCount += e / 2;
+        return pairsCount;
+    }
+}
