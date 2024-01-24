2b57ec3fee897b515da67afaa3c9a0f4c2116403
proninp <proninpaul@yandex.ru>
2024-01-24 08:55:19 +0300

    HackerRank - CSharp - 3M.Week3 - XOR Strings 3

---
+++ HackerRank/CSharp.3M-Kit/Week3/XORStrings3Task.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace Week3;
+
+/*
+ * XOR Strings 3
+ * 
+ * In this challenge, the task is to debug the existing code to successfully execute all provided test files.
+ * Given two strings consisting of digits 0 and 1 only, find the XOR of the two strings.
+ * Debug the given function Strings_xor to find the XOR of the two given strings appropriately.
+ * Note: You can modify at most three lines in the given code and you cannot add or remove lines to the code.
+ * To restore the original code, click on the icon to the right of the language selector.
+ * 
+ * Input Format
+ * The input consists of two lines. The first line of the input contains the first string, s, and the second line contains
+ * the second string, t.
+ * 
+ * Output Format
+ * Print the string obtained by the XOR of the two input strings in a single line.
+ */
+
+public class XORStrings3Task
+{
+    public static string Strings_xor(string s, string t)
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == t[i])
+                sb.Append('0');
+            else
+                sb.Append('1');
+        }
+        return sb.ToString();
+    }
+}
