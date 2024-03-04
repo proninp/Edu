e5986df277a9083f081d132a4d257645efc14667
proninp <proninpaul@yandex.ru>
2024-03-04 21:00:10 +0300

    JavaRush Level 15 tasks 2024.04.03

---
+++ JavaRush Practice/src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.javarush.task.task15.task1525.Solution.lines;
+
+public class Statics {
+    public static String FILE_NAME = "D:\\my-object-data.txt";
+}
---
+++ JavaRush Practice/src/com/javarush/task/task15/task1527/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task15.task1527;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Парсер реквестов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String url = reader.readLine();
+        String uri = url.substring(url.indexOf("?") + 1);
+        String[] params = uri.split("&");
+        StringBuilder sb = new StringBuilder();
+        List<String> objList = new ArrayList<>();
+        String paramName;
+        for(String param : params) {
+            paramName = param;
+            if (param.contains("=")) {
+                paramName = param.substring(0, param.indexOf("="));
+                if (paramName.equals("obj"))
+                    objList.add(param.substring(param.indexOf("=") + 1));
+            }
+            if (sb.length() > 0)
+                sb.append(" ");
+            sb.append(paramName);
+        }
+        System.out.println(sb);
+        for(String s : objList) {
+            Double d = null;
+            try {
+                alert(Double.parseDouble(s));
+            } catch (NumberFormatException ignored) {
+                alert(s);
+            }
+        }
+    }
+
+    public static void alert(double value) {
+        System.out.println("double: " + value);
+    }
+
+    public static void alert(String value) {
+        System.out.println("String: " + value);
+    }
+}
