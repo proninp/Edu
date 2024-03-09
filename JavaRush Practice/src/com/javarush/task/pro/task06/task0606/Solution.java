b8ecaa170c5510fb5903fcdf97558c77ea630f73
proninp <proninpaul@yandex.ru>
2024-03-09 17:38:35 +0300

    JavaRush Level 06 tasks 2024.03.09

---
+++ JavaRush Practice/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(int a){
+
+    }
+
+    public static void universalMethod(int a, int b){
+
+    }
+
+    public static void universalMethod(boolean b){
+
+    }
+
+    public static void universalMethod(boolean b, boolean bb){
+
+    }
+    public static void universalMethod(boolean b, int a){
+
+    }
+    public static void universalMethod(int a, boolean b){
+
+    }
+    public static void universalMethod(String s){
+
+    }
+    public static void universalMethod(char c){
+
+    }
+    public static void universalMethod(double d){
+
+    }
+}
---
+++ JavaRush Practice/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte bt = 1;
+    short sh = 2;
+    int i = 4;
+    long l = 8;
+    float f = 4.0f;
+    double d = 8.0;
+    boolean b = true;
+    char c = '2';
+}
