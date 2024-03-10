a177228356f66000047208c2d45bf3addc37b77c
proninp <proninpaul@yandex.ru>
2024-03-10 13:20:29 +0300

    JavaRush Level 07 tasks 2024.03.10

---
+++ JavaRush Practice/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s0 = new String("0");
+        String s1 = new String("1");
+        String s2 = new String("2");
+        String s3 = new String("3");
+        String s4 = new String("4");
+        String s5 = new String("5");
+        String s6 = new String("6");
+        String s7 = new String("7");
+        String s8 = new String("8");
+        String s9 = new String("9");
+
+        int[] a0 = new int[0];
+        int[] a1 = new int[1];
+        int[] a2 = new int[2];
+        int[] a3 = new int[3];
+        int[] a4 = new int[4];
+
+        Scanner sc0 = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+    }
+}
