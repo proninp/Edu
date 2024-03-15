c215e47d8d23a36434606490d9e21cebca22286f
proninp <proninpaul@yandex.ru>
2024-03-15 20:00:15 +0300

    JavaRush Level 10 tasks 2024.03.15

---
+++ JavaRush Practice/src/com/javarush/task/pro/task10/task1010/Iphone.java
@@ -0,0 +1,42 @@
+package com.javarush.task.pro.task10.task1010;
+
+import java.util.Objects;
+
+/* 
+Два айфона
+*/
+
+public class Iphone {
+    private String model;
+    private String color;
+    private int price;
+
+    public Iphone(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Iphone iphone = (Iphone) o;
+        if (this.model == null || this.color == null || iphone.model == null || iphone.color == null)
+            return false;
+        return price == iphone.price && model.equals(iphone.model) && color.equals(iphone.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, color, price);
+    }
+
+    public static void main(String[] args) {
+        Iphone iphone1 = new Iphone("X", "Black", 999);
+        Iphone iphone2 = new Iphone("X", "Black", 999);
+
+        System.out.println(iphone1.equals(iphone2));
+    }
+
+}
---
+++ JavaRush Practice/src/com/javarush/task/pro/task10/task1012/Memory.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task10.task1012;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/* 
+Дефрагментация памяти
+*/
+
+public class Memory {
+
+    public static void main(String[] args) {
+        String[] memory = {"object15", null, null, "object2", null, null, null, "object32", null, "object4"};
+        executeDefragmentation(memory);
+        System.out.println(Arrays.toString(memory));
+    }
+
+    public static void executeDefragmentation(String[] array) {
+        String[] copy = new String[array.length];
+        int copyIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null)
+                copy[copyIndex++] = array[i];
+        }
+        for (int i = 0; i < copy.length; i++) {
+            array[i] = copy[i];
+        }
+    }
+}
