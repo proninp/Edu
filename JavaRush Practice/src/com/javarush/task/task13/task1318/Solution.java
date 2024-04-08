7be930b9b77a2bd29fc285e3a7824f4afbd6a155
proninp <proninpaul@yandex.ru>
2024-04-08 23:38:41 +0300

    JavaRush Java Core 03 2024.04.08

---
+++ JavaRush Practice/src/com/javarush/task/task13/task1318/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task13.task1318;
+
+import java.io.*;
+import java.util.Scanner;
+
+/* 
+Чтение файла
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        scanner.close();
+
+        try (FileInputStream fileInputStream = new FileInputStream(fileName);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream))) {
+            while (reader.ready()) {
+                System.out.println(reader.readLine());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
