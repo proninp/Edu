275c488dcd3182cbdaa561e90865724fb9af8381
proninp <proninpaul@yandex.ru>
2024-04-29 21:26:05 +0300

    JavaRush Java Core 08 2024.04.29

--- JavaRush Practice/src/com/javarush/task/task18/task1819/Solution.java
+++ JavaRush Practice/src/com/javarush/task/task18/task1819/Solution.java
@@ -8,6 +8,27 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) {
-        
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String file1 = br.readLine();
+            String file2 = br.readLine();
+
+            try(BufferedInputStream bis1 = new BufferedInputStream(new FileInputStream(file1));
+                BufferedInputStream bis2 = new BufferedInputStream(new FileInputStream(file2));) {
+
+                byte[] array = new byte[bis1.available()];
+                while (bis1.available() > 0) {
+                    bis1.read(array);
+                }
+                try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file1))) {
+                    while (bis2.available() > 0) {
+                        bos.write(bis2.read());
+                    }
+                    bos.write(array);
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
