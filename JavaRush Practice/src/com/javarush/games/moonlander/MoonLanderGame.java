6c62a122a439e43db56195fb3d2f271ac10d0866
proninp <proninpaul@yandex.ru>
2024-06-11 08:29:53 +0300

    JavaRush Games - Moonlander - 05 2024.06.11

--- JavaRush Practice/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ JavaRush Practice/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -6,15 +6,18 @@ public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
     private Rocket rocket;
+    private GameObject landscape;
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
+        showGrid(false);
     }
     private void createGame() {
-        rocket = new Rocket(WIDTH / 2, 0);
+        createGameObjects();
         drawScene();
+        setTurnTimer(50);
     }
     private void drawScene() {
         for (int y = 0; y < HEIGHT; y++) {
@@ -23,5 +26,22 @@ public class MoonLanderGame extends Game {
             }
         }
         rocket.draw(this);
+        landscape.draw(this);
+    }
+    private void createGameObjects() {
+        rocket = new Rocket(WIDTH / 2, 0);
+        landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
+    }
+
+    @Override
+    public void onTurn(int step) {
+        rocket.move();
+        drawScene();
+    }
+
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
+            super.setCellColor(x, y, color);
     }
 }
