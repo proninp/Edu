8c17c2e6345fd029be02c22a0e5b070ee56b4ef1
proninp <proninpaul@yandex.ru>
2024-06-21 08:48:42 +0300

    JavaRush Games - Racer - 02 2024.06.21

--- JavaRush Practice/src/com/javarush/games/racer/RacerGame.java
+++ JavaRush Practice/src/com/javarush/games/racer/RacerGame.java
@@ -24,6 +24,22 @@ public class RacerGame extends Game {
     }
 
     private void drawField() {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                if (x == CENTER_X) {
+                    setCellColor(x, y, Color.WHITE);
+                } else if (x >= ROADSIDE_WIDTH && x < WIDTH - ROADSIDE_WIDTH) {
+                    setCellColor(x, y, Color.DIMGRAY);
+                } else {
+                    setCellColor(x, y, Color.GREEN);
+                }
+            }
+        }
+    }
 
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
+            super.setCellColor(x, y, color);
     }
 }
