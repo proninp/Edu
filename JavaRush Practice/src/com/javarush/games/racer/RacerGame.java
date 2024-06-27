59e765b3baa128a4ada9007ffc502c45acdae8ff
proninp <proninpaul@yandex.ru>
2024-06-27 08:55:13 +0300

    JavaRush Games - Racer - 04 2024.06.27

--- JavaRush Practice/src/com/javarush/games/racer/RacerGame.java
+++ JavaRush Practice/src/com/javarush/games/racer/RacerGame.java
@@ -7,6 +7,7 @@ public class RacerGame extends Game {
     public static final int HEIGHT = 64;
     public static final int CENTER_X = WIDTH / 2;
     public static final int ROADSIDE_WIDTH = 14;
+    private RoadMarking roadMarking;
 
     @Override
     public void initialize() {
@@ -16,11 +17,13 @@ public class RacerGame extends Game {
     }
 
     private void createGame() {
+        roadMarking = new RoadMarking();
         drawScene();
     }
 
     private void drawScene() {
         drawField();
+        roadMarking.draw(this);
     }
 
     private void drawField() {
