f5c252707a73de2e433bd88af7b6fa30ed227b46
proninp <velninpapro@gmail.com>
2025-09-16 18:47:30 +0300

    7kyu - String ends with?

--- CodeWars/Java/CodeWars/src/Main.java
+++ CodeWars/Java/CodeWars/src/Main.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println(NumbersThatAreAPowerOfTheirSumOfDigits.powerSumDigTerm2(20));
         System.out.println(ValidParentheses.validParentheses("((WsP.Q`))Q"));
     }
 }
---
+++ CodeWars/Java/CodeWars/src/com/codewars/kyu7/StringEndsWith.java
@@ -0,0 +1,7 @@
+﻿package com.codewars.kyu7;
+
+public class StringEndsWith {
+    public static boolean solution(String str, String ending) {
+        return str.endsWith(ending);
+    }
+}
