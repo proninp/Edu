b59e9ac842cdcde1a06c37b57d1b5331e0ce9752
proninp <velninpapro@gmail.com>
2025-09-17 08:40:16 +0300

    7kyu - Complementary DNA

---
+++ CodeWars/Java/CodeWars/src/main/java/com/codewars/kyu7/сomplementaryDNA/DnaStrand.java
@@ -0,0 +1,24 @@
+﻿package main.java.com.codewars.kyu7.сomplementaryDNA;
+
+public class DnaStrand {
+    public static String makeComplement(String dna) {
+        var sb = new StringBuilder(dna.length());
+        for (int i = 0; i < dna.length(); i++) {
+            switch (dna.charAt(i)) {
+                case 'A':
+                    sb.append('T');
+                    break;
+                case 'T':
+                    sb.append('A');
+                    break;
+                case 'C':
+                    sb.append('G');
+                    break;
+                case 'G':
+                    sb.append('C');
+                    break;
+            }
+        }
+        return sb.toString();
+    }
+}
