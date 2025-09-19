c6569ef089fab0be0c6911d35a2a023f3b918770
proninp <velninpapro@gmail.com>
2025-09-19 09:46:13 +0300

    6kyu - Consecutive strings

---
+++ CodeWars/Java/CodeWars/src/main/java/com/codewars/kyu6/consecutiveStrings/ConsecutiveStrings.java
@@ -0,0 +1,18 @@
+﻿package main.java.com.codewars.kyu6.consecutiveStrings;
+
+// https://www.codewars.com/kata/56a5d994ac971f1ac500003e/
+public class ConsecutiveStrings {
+    public static String longestConsec(String[] strarr, int k) {
+        var result = "";
+        for (int i = 0; i < strarr.length - k + 1; i++) {
+            var sb = new StringBuilder();
+            for (int j = i; j < i + k; j++) {
+                sb.append(strarr[j]);
+            }
+            if (result.length() < sb.length()) {
+                result = sb.toString();
+            }
+        }
+        return result;
+    }
+}
