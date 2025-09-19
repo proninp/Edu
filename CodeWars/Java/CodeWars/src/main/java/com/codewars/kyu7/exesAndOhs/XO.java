6da3b2fe98582f35d295128213996547ed3255b0
proninp <velninpapro@gmail.com>
2025-09-19 08:42:42 +0300

    7kyu - Exes and Ohs

---
+++ CodeWars/Java/CodeWars/src/main/java/com/codewars/kyu7/exesAndOhs/XO.java
@@ -0,0 +1,16 @@
+﻿package main.java.com.codewars.kyu7.exesAndOhs;
+
+public class XO {
+    public static boolean getXO (String str) {
+        var score = 0;
+        for (int i = 0; i < str.length(); i++) {
+            var c = Character.toLowerCase(str.charAt(i));
+            if (c == 'x') {
+                score++;
+            } else if (c == 'o') {
+                score--;
+            }
+        }
+        return score == 0;
+    }
+}
